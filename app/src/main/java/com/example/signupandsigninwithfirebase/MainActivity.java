67c2145129493998647a9b202c681d52963c49c7
Abdullah272056 <abdullah272056@gmail.com>
2020-11-06 23:09:23 +0600

    signIn function add

--- app/src/main/java/com/example/signupandsigninwithfirebase/MainActivity.java
+++ app/src/main/java/com/example/signupandsigninwithfirebase/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.signupandsigninwithfirebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -11,17 +12,28 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     EditText signInEmailEditText,signInPasswordEditText;
     Button signInButton;
     TextView signUpTextView;
     ProgressBar progressBar;
+
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.setTitle("Sign In Activity");
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
 
         //view finding
         signInEmailEditText=findViewById(R.id.signInEmailEditTextId);
@@ -75,10 +87,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             return;
         }
         progressBar.setVisibility(View.VISIBLE);
-        
+
         // add sign In function
 
+        mAuth.signInWithEmailAndPassword(signInEmail, signInPassword)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressBar.setVisibility(View.GONE);
+                        if (task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(MainActivity.this, "Login not Successful", Toast.LENGTH_SHORT).show();
 
-        progressBar.setVisibility(View.GONE);
+                        }
+
+                    }
+                });
+
+
     }
 }
