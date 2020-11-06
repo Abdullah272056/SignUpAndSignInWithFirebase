93f1e32529db6287a485f4fb258d13067d9f7e2d
Abdullah272056 <abdullah272056@gmail.com>
2020-11-06 16:43:27 +0600

    MainActivity finding view

--- app/src/main/java/com/example/signupandsigninwithfirebase/MainActivity.java
+++ app/src/main/java/com/example/signupandsigninwithfirebase/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.signupandsigninwithfirebase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText signInEmailEditText,signInPasswordEditText;
+    Button signInButton;
+    TextView signUpTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //view finding
+        signInEmailEditText=findViewById(R.id.signInEmailEditTextId);
+        signInPasswordEditText=findViewById(R.id.signInPasswordEditTextId);
+        signInButton=findViewById(R.id.signInButtonId);
+        signUpTextView=findViewById(R.id.signUpTextViewId);
     }
 }
